f759238bf4af7c833089a7f77dc25aedbb404daa
abdallahemish <121138618+abdallahemish@users.noreply.github.com>
2022-12-23 16:03:06 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args) // this function defines the entry point
+        {
+            bool Prime = true;
+            Console.WriteLine("Prime Numbers between 1 to 100 : ");
+            for (int a = 2; a <= 100; a++) //upper limit and lower limit are defined
+            {
+                for (int b = 2; b <= 100; b++)// base logic for the primality
+                {
+                    if (a != b && a % b == 0) //modulo operators employed
+                    {
+                        Prime = false;
+                        break;
+                    }
+                }
+                if (Prime)
+                {
+                    Console.Write("\t" + a); //printing the correct variable
+                }
+                Prime = true;
+            }
+            Console.ReadKey(); //hold the output screen
+        }
+    }
+}
